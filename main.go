4cd9f3fa035e72745dd88a503c05e5b8b42a031d
electr1fy0 <electr1fy0@icloud.com>
2025-10-18 22:30:29 +0530

    json fixes

--- main.go
+++ main.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 	"os/exec"
+	"time"
 )
 
-func main() {
+type ClipboardData struct {
+	Text string `json:"text"`
+}
 
-	// Define the command and its arguments
-	cmd := exec.Command("wl-paste")
+var clipboard []ClipboardData
 
-	// Execute the command and capture its standard output
-	out, err := cmd.Output()
+func updateJSON(lastCopied string) {
+	filename := "copied.json"
+	jsonData, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatalf("Failed to execute command: %v", err)
+		log.Fatalf("Failed to marshal JSON: %v", err)
 	}
+	err = json.Unmarshal(jsonData, &clipboard)
+	if err != nil {
+		log.Fatalln("failed to unmarshal json:", err)
+	}
+	if clipboard[len(clipboard)-1].Text != lastCopied {
+		clipboard = append(clipboard, ClipboardData{Text: lastCopied})
+	}
+	newJson, err := json.Marshal(clipboard)
+	if err != nil {
+		log.Fatalf("failed to unmarshal json:", err)
+	}
+	err = os.WriteFile(filename, newJson, 0644)
+	if err != nil {
+		log.Fatalln("failed to write to file:", err)
+	}
+	log.Println("File written successfully.")
 
-	saveToJSON(string(out))
+}
+
+func main() {
+	for {
+		cmd := exec.Command("wl-paste")
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatalln("failed to execute wl-paste command:", err)
+		}
+		updateJSON(string(output))
+		time.Sleep(5 * time.Second)
+	}
 
 }
